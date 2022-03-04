ea7f140b0f3bf83d385eab0e33e570d9202c7aa9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-03-03 23:18:47 -0800

    Make the error properties public

--- src/error.rs
+++ src/error.rs
@@ -81,18 +81,18 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// # }
 /// ```
 pub struct Error {
-    messages: Vec<ErrorMessage>,
+    pub messages: Vec<ErrorMessage>,
 }
 
-struct ErrorMessage {
+pub struct ErrorMessage {
     // Span is implemented as an index into a thread-local interner to keep the
     // size small. It is not safe to access from a different thread. We want
     // errors to be Send and Sync to play nicely with the Failure crate, so pin
     // the span we're given to its original thread and assume it is
     // Span::call_site if accessed from any other thread.
-    start_span: ThreadBound<Span>,
-    end_span: ThreadBound<Span>,
-    message: String,
+    pub start_span: ThreadBound<Span>,
+    pub end_span: ThreadBound<Span>,
+    pub message: String,
 }
 
 #[cfg(test)]
